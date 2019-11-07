e36416d13778ed123b1dead8fcf60904856eb435
godanan91134 <55218484+godanan91134@users.noreply.github.com>
2019-11-07 11:47:28 +0800

    12345

---
+++ UnityProject/Assets/ro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ro : MonoBehaviour
+{
+
+    [Header("任務")]
+
+    public string star = "ㄟ你過來一下";
+
+    [Header("速度")]
+
+    public float talkspeed = 5f;
+
+    [Header("任務")]
+
+    public bool mission_complete = false;
+
+    public int count_player = 0;
+
+    public int finish = 5;
+
+
+
+    void Start()
+
+    {
+
+        Debug.Log(star);
+
+    }
+
+}
